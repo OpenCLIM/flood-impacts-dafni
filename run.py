0ca843883822e0fcf43816db2e3fe8ce190f148c
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2020-09-16 13:06:23 +0100

    joining paths and opening memory file with context manager

--- run.py
+++ run.py
@@ -5,37 +5,43 @@ import numpy as np
 from rasterio.transform import Affine
 from rasterstats import zonal_stats as zs
 import rasterio as rio
+import os
 
-data = pd.read_csv('/data/inputs/run/R1C1_SurfaceMaps/R1_C1_max_depth.csv')
+data_path = '/data'
+inputs_path = os.path.join(data_path, 'inputs')
+outputs_path = os.path.join(data_path, 'outputs')
 
-# GDAL XYZ driver not working so creating raster manually
-unique_x, x_inverse = np.unique(data['XCen'].values, return_inverse=True)
-unique_y, y_inverse = np.unique(data['YCen'].values, return_inverse=True)
+data = pd.read_csv(os.path.join(inputs_path, 'run/R1C1_SurfaceMaps/R1_C1_max_depth.csv'))
+
+# GDAL XYZ driver does not represent missing data properly so converting manually
+unique_x, x_inverse = np.unique(data.XCen.values, return_inverse=True)
+unique_y, y_inverse = np.unique(data.YCen.values, return_inverse=True)
 x_size, y_size = len(unique_x), len(unique_y)
 y_inverse = y_size - y_inverse - 1
-missing_data = -9999
-depth = np.full((y_size, x_size), missing_data, dtype=float)
+nodata = -9999
+depth = np.full((y_size, x_size), nodata, dtype=float)
 depth[y_inverse, x_inverse] = data.Depth.values
 res = unique_x[1] - unique_x[0]
-f = MemoryFile()
-with rio.open(
-    f,
-    'w',
-    driver='GTiff',
-    height=y_size,
-    width=x_size,
-    count=1,
-    dtype=depth.dtype,
-    transform=Affine.translation(data.XCen.min(), data.YCen.max()) * Affine.scale(res, -res),
-    nodata=missing_data
-) as dst:
-    dst.write(depth, 1)
 
-with f.open() as src:
-    buildings = gpd.read_file('/data/inputs/mastermap/mastermap-topo_3629050_0.gpkg', bbox=src.bounds,
-                              layer='TopographicArea')
+with MemoryFile() as f:
+    with rio.open(
+        f,
+        'w',
+        driver='GTiff',
+        height=y_size,
+        width=x_size,
+        count=1,
+        dtype=depth.dtype,
+        transform=Affine.translation(data.XCen.min(), data.YCen.max()) * Affine.scale(res, -res),
+        nodata=nodata
+    ) as dst:
+        dst.write(depth, 1)
 
-    buildings['depth'] = [row['max'] for row in zs(buildings, src.read(1), affine=src.transform, stats=['max'],
-                                                   all_touched=True, nodata=missing_data)]
+    with f.open() as src:
+        buildings = gpd.read_file(os.path.join(inputs_path, 'mastermap/mastermap-topo_3629050_0.gpkg'), bbox=src.bounds,
+                                  layer='TopographicArea')
 
-    buildings.to_file('/data/outputs/building-depths.gpkg', driver='GPKG')
+        buildings['depth'] = [row['max'] for row in zs(buildings, src.read(1), affine=src.transform, stats=['max'],
+                                                       all_touched=True, nodata=nodata)]
+
+        buildings.to_file(os.path.join(outputs_path, 'building-depths.gpkg'), driver='GPKG')
