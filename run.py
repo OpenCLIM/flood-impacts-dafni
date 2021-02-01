f3cc9ba40e8e5aa8be6dfbd21d445b76a1ab94c7
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2021-02-01 09:12:09 +0000

    Calculate line inundated distances

--- run.py
+++ run.py
@@ -1,45 +1,56 @@
-import pandas as pd
 import geopandas as gpd
 from rasterstats import zonal_stats as zs
 import rasterio as rio
 import os
+from rasterio import features
+import numpy as np
+from shapely.geometry import shape
+import glob
 
 # Define paths
 data_path = os.getenv('DATA_PATH', '/data')
 inputs_path = os.path.join(data_path, 'inputs')
 outputs_path = os.path.join(data_path, 'outputs')
-mastermap = os.path.join(inputs_path, 'mastermap/mastermap-topo_3889921.gpkg')
+mastermap = glob.glob(os.path.join(inputs_path, 'mastermap', '*.gpkg'))[0]
 area_layer = 'Topographicarea'
 line_layer = 'Topographicline'
-output_file = os.path.join(outputs_path, 'depths.gpkg')
+output_file = os.path.join(outputs_path, 'features.gpkg')
 
 threshold = os.getenv('THRESHOLD', 0.01)
 
 buffer = 5
 
 with rio.open(os.path.join(inputs_path, 'run/max_depth.tif')) as src:
-    # Read buildings data
-
+    # Read MasterMap data
     areas = gpd.read_file(mastermap, bbox=src.bounds, layer=area_layer).rename(columns={'fid': 'toid'})
     lines = gpd.read_file(mastermap, bbox=src.bounds, layer=line_layer).rename(columns={'fid': 'toid'})
+
+    # Read flood depths
     depth = src.read(1)
 
-    # Extract maximum depths for each building from CityCAT results
+    # Extract maximum depths for each building
     areas['depth'] = [row['max'] for row in zs(areas.buffer(buffer), depth, affine=src.transform, stats=['max'],
                                                all_touched=True, nodata=src.nodata)]
 
-    lines['depth'] = [row['max'] for row in zs(lines.buffer(buffer), depth, affine=src.transform, stats=['max'],
-                                               all_touched=True, nodata=src.nodata)]
+    # Find areas flooded above threshold
+    flooded_areas = gpd.GeoDataFrame(
+        geometry=[shape(s[0]) for s in features.shapes(
+            np.ones(depth.shape, dtype=rio.uint8), mask=depth >= threshold, transform=src.transform)], crs=src.crs)
 
+    # Filter areas and intersect lines
     areas = areas[areas.depth >= threshold]
-    lines = lines[lines.depth >= threshold]
+    lines = gpd.overlay(lines, flooded_areas)
 
-    # Save a copy of buildings data with a new depth field
+    # Save a copy of lines and areas to the GeoPackage
     if len(areas) > 0:
         areas.to_file(output_file, layer=area_layer, driver='GPKG')
     if len(lines) > 0:
         lines.to_file(output_file, layer=line_layer, driver='GPKG')
 
-    df = pd.concat([areas, lines])
-    df.to_csv(os.path.join(outputs_path, 'depths.csv'), index=False)
-    df.featurecode.value_counts().to_csv(os.path.join(outputs_path, 'counts.csv'))
+    # Count the number of areas by featurecode and save to CSV
+    areas.featurecode.value_counts().rename_axis('featurecode').rename('count').to_csv(
+        os.path.join(outputs_path, 'counts.csv'))
+
+    # Count the total inundated distances by featurecode and save to CSV
+    lines.geometry.length.groupby(lines.featurecode).sum().round(2).rename('distance (m)').to_csv(
+        os.path.join(outputs_path, 'distance.csv'))
