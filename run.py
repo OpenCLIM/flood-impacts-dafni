cfe5322cf434a1790fe90660a768449f1b4e6bca
Fergus McClean <fergus.mcclean@newcastle.ac.uk>
2021-06-17 12:13:58 +0100

    Pre-filter buildings, fix nodata issue, calculate flooded perimeter

--- run.py
+++ run.py
@@ -19,6 +19,7 @@ output_file = os.path.join(outputs_path, 'features.gpkg')
 
 threshold = float(os.getenv('THRESHOLD'))
 
+# Set buffer around buildings
 buffer = 5
 
 with rio.open(os.path.join(inputs_path, 'run/max_depth.tif')) as max_depth,\
@@ -34,21 +35,33 @@ with rio.open(os.path.join(inputs_path, 'run/max_depth.tif')) as max_depth,\
     depth = max_depth.read(1)
     vd_product = max_vd_product.read(1)
 
-
-    # Extract maximum depth and vd_product for each building
-    buildings['depth'] = [row['max'] for row in zs(buildings.buffer(buffer), depth, affine=max_depth.transform, stats=['max'],
-                                                   all_touched=True, nodata=max_depth.nodata)]
-
-    buildings['vd_product'] = [row['max'] for row in zs(buildings.buffer(buffer), vd_product, affine=max_vd_product.transform, stats=['max'],
-                                                        all_touched=True, nodata=max_vd_product.nodata)]
-
     # Find flooded areas
     flooded_areas = gpd.GeoDataFrame(
         geometry=[shape(s[0]) for s in features.shapes(
-            np.ones(depth.shape, dtype=rio.uint8), mask=depth >= threshold, transform=max_depth.transform)], crs=max_depth.crs)
+            np.ones(depth.shape, dtype=rio.uint8), mask=np.logical_and(depth >= threshold, max_depth.read_masks(1)),
+            transform=max_depth.transform)], crs=max_depth.crs)
+
+    # Buffer buildings
+    buildings['geometry'] = buildings.buffer(buffer)
 
     # Filter buildings
-    buildings = buildings[buildings.depth >= threshold]
+    buildings = buildings[buildings.intersects(flooded_areas.unary_union)]
 
-    # Count the number of buildings by featurecode and save to CSV
-    buildings[['toid', 'depth',  'vd_product']].to_csv(os.path.join(outputs_path, 'buildings.csv'), index=False)
+    # Extract maximum depth and vd_product for each building
+    buildings['depth'] = [row['max'] for row in
+                          zs(buildings, depth, affine=max_depth.transform, stats=['max'],
+                             all_touched=True, nodata=max_depth.nodata)]
+
+    buildings['vd_product'] = [row['max'] for row in
+                               zs(buildings, vd_product, affine=max_vd_product.transform, stats=['max'],
+                                  all_touched=True, nodata=max_vd_product.nodata)]
+
+    # Get the flooded perimeter length for each building
+    flooded_perimeter = gpd.overlay(gpd.GeoDataFrame({'toid': buildings.toid}, geometry=buildings.geometry.boundary,
+                                                     crs=buildings.crs), flooded_areas)
+    flooded_perimeter['flooded_perimeter'] = flooded_perimeter.geometry.length.round(2)
+
+    buildings = buildings.merge(flooded_perimeter, on='toid')[['toid', 'depth',  'vd_product', 'flooded_perimeter']]
+
+    # Save to CSV
+    buildings.to_csv(os.path.join(outputs_path, 'buildings.csv'), index=False)
